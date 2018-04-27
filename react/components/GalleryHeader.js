b9067724d37ddffff2550aa7a0042db5401fc5ac
Felipe Sales <sales.felipeb@gmail.com>
2018-04-27 17:15:33 -0300

    First version of sortProducts

--- react/components/GalleryHeader.js
+++ react/components/GalleryHeader.js
@@ -7,24 +7,15 @@ import { FormattedMessage, injectIntl } from 'react-intl'
 
 class GalleryHeader extends React.Component {
   sortingOptions() {
-    const sortingOptions = [
-      'sortBy.relevance',
-      'sortBy.higherPrice',
-      'sortBy.lowerPrice',
-      'sortBy.nameAZ',
-      'sortBy.nameZA',
-      'sortBy.bestSellers',
-    ]
-
     const context = this.context
 
-    return sortingOptions.map(el => {
+    return this.props.sortingOptions.map(el => {
       return { value: el, label: context.intl.formatMessage({ id: el }) }
     })
   }
 
   render() {
-    const { quantity, query } = this.props
+    const { quantity, query, selectedSort, onSortChange } = this.props
     const options = this.sortingOptions()
 
     return (
@@ -49,7 +40,12 @@ class GalleryHeader extends React.Component {
           />
         </div>
         <div className="w-20-l w-20-m w-100-s pa3">
-          <Dropdown size="large" options={options} value={options[0].value} />
+          <Dropdown
+            size="large"
+            options={options}
+            value={options[selectedSort].value}
+            onChange={onSortChange}
+          />
         </div>
       </div>
     )
@@ -67,6 +63,12 @@ GalleryHeader.propTypes = {
   query: PropTypes.shape({
     search: PropTypes.string.isRequired,
   }),
+  /** Function that will be called when the user change the sort method */
+  onSortChange: PropTypes.func.isRequired,
+  /** Wich sorting method is selected */
+  selectedSort: PropTypes.number.isRequired,
+  /** List of sorting methods*/
+  sortingOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default injectIntl(GalleryHeader)
