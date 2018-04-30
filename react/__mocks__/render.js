b807cbb22f8f71700ba9f8c0590bdc3c59c9a3f8
Felipe Sales <sales.felipeb@gmail.com>
2018-04-30 16:05:30 -0300

    Basic tests

---
+++ react/__mocks__/render.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+/**
+ * ExtensionPoint Mocked Component.
+ */
+export class ExtensionPoint extends Component {
+  render() {
+    return <div>Extension Point</div>
+  }
+}
