6035f70c36ad25f75f682062aa782c6ce3b492a2
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-07-27 09:58:37 -0300

    Refactor gallery to sfc

--- react/components/Gallery.js
+++ react/components/Gallery.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { PropTypes } from 'prop-types'
 import classNames from 'classnames'
 
@@ -8,49 +8,46 @@ import GalleryItem from './GalleryItem'
 /**
  * Canonical gallery that displays a list of given products.
  */
-export default class Gallery extends Component {
-  static propTypes = {
-    /** Maximum number of items per line. */
-    maxItemsPerLine: PropTypes.number.isRequired,
-    /** Maximum number of items per page. */
-    maxItemsPerPage: PropTypes.number.isRequired,
-    /** Products to be displayed. */
-    products: PropTypes.arrayOf(productShape),
-    /** ProductSummary props. */
-    summary: PropTypes.any,
-  }
+const Gallery = props => {
+  const { maxItemsPerLine } = props
 
-  static defaultProps = {
-    maxItemsPerLine: 5,
-    maxItemsPerPage: 10,
-    products: [],
-  }
+  const itemClassName = classNames('vtex-gallery__item flex mv2 pa1 fl', {
+    'w-100 w-50-m w-20-l': maxItemsPerLine === 5,
+    'w-100 w-50-m w-25-l': maxItemsPerLine === 4,
+    'w-100 w-50-m w-third-l': maxItemsPerLine === 3,
+  })
 
-  getWidthClass() {
-  }
-
-  render() {
-    const { maxItemsPerLine } = this.props
-
-    const itemClassName = classNames('vtex-gallery__item flex mv2 pa1 fl', {
-      'w-100 w-50-m w-20-l': maxItemsPerLine === 5,
-      'w-100 w-50-m w-25-l': maxItemsPerLine === 4,
-      'w-100 w-50-m w-third-l': maxItemsPerLine === 3,
-    })
-
-    return (
-      <div className="vtex-gallery pa3 w-100 flex flex-wrap justify-start">
-        {this.props.products.map(item => {
-          return (
-            <div
-              key={item.productId}
-              className={itemClassName}
-            >
-              <GalleryItem item={item} summary={this.props.summary} />
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
+  return (
+    <div className="vtex-gallery pa3 w-100 flex flex-wrap justify-start">
+      {this.props.products.map(item => {
+        return (
+          <div
+            key={item.productId}
+            className={itemClassName}
+          >
+            <GalleryItem item={item} summary={props.summary} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
+
+Gallery.propTypes = {
+  /** Maximum number of items per line. */
+  maxItemsPerLine: PropTypes.number.isRequired,
+  /** Maximum number of items per page. */
+  maxItemsPerPage: PropTypes.number.isRequired,
+  /** Products to be displayed. */
+  products: PropTypes.arrayOf(productShape),
+  /** ProductSummary props. */
+  summary: PropTypes.any,
+}
+
+Gallery.defaultProps = {
+  maxItemsPerLine: 5,
+  maxItemsPerPage: 10,
+  products: [],
+}
+
+export default Gallery
