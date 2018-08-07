8f804531f735a5ddc14754d1dd00640e66231a97
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-08-07 08:29:32 -0300

    Refact to SFC

--- react/components/AvailableFilters.js
+++ react/components/AvailableFilters.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import SearchFilter from './SearchFilter'
 import { mountOptions } from '../constants/SearchHelpers'
 
-export default class AvailableFilters extends Component {
-  static propTypes = {
-    filters: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string,
-      type: PropTypes.string,
-      rest: PropTypes.string,
-      oneSelectedCollapse: PropTypes.bool,
-    })),
-    map: PropTypes.string,
+const AvailableFilters = ({ filters, map, rest, getLinkProps }) => (
+  filters.map(filter => {
+    const { type, title, options, oneSelectedCollapse = false } = filter
+
+    return (
+      <SearchFilter
+        key={title}
+        title={title}
+        options={mountOptions(options, type, map, rest)}
+        oneSelectedCollapse={oneSelectedCollapse}
+        type={type}
+        getLinkProps={getLinkProps}
+      />
+    )
+  })
+)
+
+AvailableFilters.propTypes = {
+  filters: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    type: PropTypes.string,
     rest: PropTypes.string,
-    getLinkProps: PropTypes.func,
-  }
-
-  static defaultProps = {
-    filters: [],
-  }
-
-  render() {
-    const { filters, map, rest, getLinkProps } = this.props
-
-    return filters.map(filter => {
-      const { type, title, options, oneSelectedCollapse = false } = filter
-
-      return (
-        <SearchFilter
-          key={title}
-          title={title}
-          options={mountOptions(options, type, map, rest)}
-          oneSelectedCollapse={oneSelectedCollapse}
-          type={type}
-          getLinkProps={getLinkProps}
-        />
-      )
-    })
-  }
+    oneSelectedCollapse: PropTypes.bool,
+  })),
+  map: PropTypes.string,
+  rest: PropTypes.string,
+  getLinkProps: PropTypes.func,
 }
 
+AvailableFilters.defaultProps = {
+  filters: [],
+}
+
+export default AvailableFilters
