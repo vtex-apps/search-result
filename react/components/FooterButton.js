7710ee95965acf5745213079bc8c23682efe9010
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-08-08 16:44:07 -0300

    Change color on hover

--- react/components/FooterButton.js
+++ react/components/FooterButton.js
@@ -5,7 +5,7 @@ export default function FooterButton({ children, tag, ...props }) {
   const Tag = tag || 'button'
 
   return (
-    <Tag {...props} className="vtex-footer-button link pointer f5 bg-dark-gray br1 white ba b--white pv2 ph7">
+    <Tag {...props} className="vtex-footer-button link pointer f5 bg-dark-gray br1 white ba b--white pv2 ph7 hover-bg-white hover-dark-gray">
       {children}
     </Tag>
   )
