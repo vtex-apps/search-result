1e77ab7966a6bc7d17abff3edbb4bd6ac6510783
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-08-22 11:44:36 -0300

    Remove footer of filter tab and move filter action to item onClick

--- react/components/AccordionFilterContainer.js
+++ react/components/AccordionFilterContainer.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'render'
 import { injectIntl, intlShape } from 'react-intl'
-import { flatten, map, filter, pipe } from 'ramda'
 
 import Popup from './Popup'
-import FooterButton from './FooterButton'
 import AccordionFilterItem from './AccordionFilterItem'
 import { mountOptions } from '../constants/SearchHelpers'
 
@@ -20,18 +17,6 @@ class AccordionFilterContainer extends Component {
 
   state = {
     openedItem: null,
-    selectedOptions: pipe(
-      map(filter =>
-        mountOptions(filter.options, filter.type, this.props.map, this.props.rest),
-      ),
-      flatten,
-      filter(option => option.selected),
-      map(option => ({
-        ...option,
-        name: option.Name,
-        link: option.Link,
-      }))
-    )(this.props.filters || []),
   }
 
   handleOpen = id => e => {
@@ -48,110 +33,40 @@ class AccordionFilterContainer extends Component {
     }
   }
 
-  handleClean = e => {
-    e.preventDefault()
-
-    this.setState({
-      selectedOptions: [],
-    })
-  }
-
-  isTypeActive = opt => (
-    !!this.state.selectedOptions.find(e => e.type === opt.type)
-  )
-
-  isOptionActive = opt => (
-    !!this.state.selectedOptions.find(e => e.Name === opt.Name)
-  )
-
-  handleSelectOption = (e, option) => {
-    e.preventDefault()
-
-    if ((option.oneSelectedCollapse && !this.isTypeActive(option)) ||
-       (!option.oneSelectedCollapse && !this.isOptionActive(option))) {
-      this.setState({
-        selectedOptions: [
-          ...this.state.selectedOptions,
-          {
-            ...option,
-            link: option.Link,
-            name: option.Name,
-          },
-        ],
-      })
-    } else if (
-      option.oneSelectedCollapse &&
-      this.isTypeActive(option) &&
-      !this.isOptionActive(option)
-    ) {
-      this.setState({
-        selectedOptions: this.state.selectedOptions.map(
-          e => e.type === option.type ? option : e
-        ),
-      })
-    } else {
-      this.setState({
-        selectedOptions:
-          this.state.selectedOptions.filter(opt => opt.Name !== option.Name),
-      })
-    }
-  }
-
   render() {
     const { filters, intl, getLinkProps, map, rest } = this.props
-    const { openedItem, selectedOptions } = this.state
+    const { openedItem } = this.state
 
-    const linkProps = getLinkProps(selectedOptions, true)
+    const nonEmptyFilters = filters.filter(spec => spec.options.length > 0)
 
     return (
       <Popup
         title={intl.formatMessage({ id: 'search-result.filter-button.title' })}
         id="filters"
-        renderFooter={({ onClose }) => null/*(
-          <div className="flex justify-between pv3 ph6">
-            <FooterButton onClick={this.handleClean}>
-              {intl.formatMessage({ id: 'search-result.clear-filters.title' })}
-            </FooterButton>
-            <div className="bg-white self-stretch" style={{ width: 1 }} />
-            <FooterButton
-              tag={Link}
-              page={linkProps.page}
-              params={linkProps.params}
-              query={linkProps.queryString}
-              onClick={e => {
-                onClose(e)
-
-                this.setState({
-                  openedItem: null,
-                })
-              }}
-            >
-              {intl.formatMessage({ id: 'search-result.filter-action.title' })}
-            </FooterButton>
-          </div>
-        )*/}
       >
-        <div className="vtex-accordion-filter">
-          {filters.map(filter => {
-            const { type, title, options, oneSelectedCollapse } = filter
-            const isOpen = openedItem === filter.title
+        {({ onClose }) => (
+          <div className="vtex-accordion-filter">
+            {nonEmptyFilters.map(filter => {
+              const { type, title, options, oneSelectedCollapse } = filter
+              const isOpen = openedItem === filter.title
 
-            return (
-              <AccordionFilterItem
-                key={filter.title}
-                type={type}
-                title={title}
-                options={mountOptions(options, type, map, rest)}
-                open={isOpen}
-                show={openedItem === null ? true : isOpen}
-                onOpen={this.handleOpen(filter.title)}
-                onSelectOption={this.handleSelectOption}
-                isOptionActive={this.isOptionActive}
-                oneSelectedCollapse={oneSelectedCollapse}
-              />
-            )
-          })}
-        </div>
+              return (
+                <AccordionFilterItem
+                  key={filter.title}
+                  type={type}
+                  title={title}
+                  options={mountOptions(options, type, map, rest)}
+                  open={isOpen}
+                  show={openedItem === null ? true : isOpen}
+                  onOpen={this.handleOpen(filter.title)}
+                  onItemSelected={onClose}
+                  oneSelectedCollapse={oneSelectedCollapse}
+                  getLinkProps={getLinkProps}
+                />
+              )
+            })}
+          </div>
+        )}
       </Popup>
     )
   }
--- react/components/AccordionFilterItem.js
+++ react/components/AccordionFilterItem.js
@@ -1,99 +1,93 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
 import classNames from 'classnames'
+import { Link } from 'render'
 
 import Arrow from '../images/Arrow'
 import CheckTick from '../images/CheckTick'
 import { facetOptionShape } from '../constants/propTypes'
 import { getFilterTitle } from '../constants/SearchHelpers'
 
-class AccordionFilterItem extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    options: PropTypes.arrayOf(facetOptionShape),
-    type: PropTypes.string,
-    show: PropTypes.bool,
-    open: PropTypes.bool,
-    oneSelectedCollapse: PropTypes.bool,
-    onOpen: PropTypes.func,
-    onSelectOption: PropTypes.func,
-    isOptionActive: PropTypes.func,
-    intl: intlShape,
-  }
+const AccordionFilterItem = ({
+  title,
+  options,
+  type,
+  show,
+  open,
+  onOpen,
+  onItemSelected,
+  getLinkProps,
+  intl,
+  oneSelectedCollapse,
+}) => (
+  <Fragment>
+    <div
+      className={classNames('vtex-accordion-filter__item fw3 pv3 ph7 pointer bb b--light-gray', {
+        'vtex-accordion-filter__item--active': open,
+        'vtex-accordion-filter__item--hidden dn': !show,
+      })}
+      onClick={onOpen}
+    >
+      <div
+        className={classNames('vtex-accordion-filter__item-title', {
+          'normal dark-gray': open,
+        })}
+      >
+        {getFilterTitle(title, intl)}
 
-  render() {
-    const {
-      title,
-      options,
-      type,
-      show,
-      open,
-      onOpen,
-      onSelectOption,
-      isOptionActive,
-      intl,
-      oneSelectedCollapse,
-    } = this.props
+        <span className="vtex-accordion-filter__item-icon fr">
+          <Arrow up={open} size={10} />
+        </span>
+      </div>
+    </div>
+    {open && (
+      <div className="vtex-accordion-filter__item-options">
+        {options.map(opt => {
+          const pagesArgs = getLinkProps({
+            ...opt,
+            name: opt.Name,
+            link: opt.Link,
+            path: opt.path,
+            type,
+            oneSelectedCollapse,
+          })
 
-    return (
-      <Fragment>
-        <div
-          className={classNames('vtex-accordion-filter__item fw3 pv3 ph7 pointer bb b--light-gray', {
-            'vtex-accordion-filter__item--active': open,
-            'vtex-accordion-filter__item--hidden dn': !show,
-          })}
-          onClick={onOpen}
-        >
-          <div
-            className={classNames('vtex-accordion-filter__item-title', {
-              'normal dark-gray': open,
-            })}
-          >
-            {getFilterTitle(title, intl)}
+          return (
+            <Link
+              key={opt.Name}
+              className="vtex-accordion-filter__item-opt pv3 ph7 pointer bb b--light-gray link fw3 db dark-gray"
+              page={pagesArgs.page}
+              params={pagesArgs.params}
+              query={pagesArgs.queryString}
+              onClick={onItemSelected}
+            >
+              {opt.Name}
 
-            <span className="vtex-accordion-filter__item-icon fr">
-              <Arrow up={open} size={10} />
-            </span>
-          </div>
-        </div>
-        {open && (
-          <div className="vtex-accordion-filter__item-options">
-            {options.map(opt => {
-              const isActive = isOptionActive(opt)
+              {false && (
+                <span className="vtex-accordion-filter__check-icon fr">
+                  <CheckTick />
+                </span>
+              )}
+            </Link>
+          )
+        })}
+      </div>
+    )}
+  </Fragment>
+)
 
-              const optionClassName = classNames('vtex-accordion-filter__item-opt pv3 ph7 pointer bb b--light-gray', {
-                'vtex-accordion-filter__item-opt--active dark-gray normal': isActive,
-                'fw3': !isActive,
-              })
-
-              const filterOption = {
-                type,
-                oneSelectedCollapse,
-                ...opt,
-              }
-
-              return (
-                <div
-                  key={opt.Name}
-                  className={optionClassName}
-                  onClick={e => onSelectOption(e, filterOption)}
-                >
-                  {opt.Name}
-
-                  {isActive && (
-                    <span className="vtex-accordion-filter__check-icon fr">
-                      <CheckTick />
-                    </span>
-                  )}
-                </div>
-              )
-            })}
-          </div>
-        )}
-      </Fragment>
-    )
-  }
+AccordionFilterItem.propTypes = {
+  title: PropTypes.string,
+  options: PropTypes.arrayOf(facetOptionShape),
+  type: PropTypes.string,
+  show: PropTypes.bool,
+  open: PropTypes.bool,
+  oneSelectedCollapse: PropTypes.bool,
+  onOpen: PropTypes.func,
+  onItemSelected: PropTypes.func,
+  getLinkProps: PropTypes.func,
+  intl: intlShape,
 }
 
 export default injectIntl(AccordionFilterItem)
