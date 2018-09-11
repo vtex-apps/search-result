92263b09d133e65fdc7951b792a724da6daf35bb
Felipe Sales <sales.felipeb@gmail.com>
2018-09-11 14:18:38 -0300

    Splitting SearchResult using the Container/Component strategie

--- react/index.js
+++ react/index.js
@@ -4,8 +4,15 @@ import React, { Component } from 'react'
 import QueryString from 'query-string'
 import ProductSummary from 'vtex.product-summary/index'
 
+import SearchResult from './components/SearchResult'
 import { SORT_OPTIONS } from './components/OrderBy'
+<<<<<<< HEAD
 import { PopupProvider } from './components/Popup'
+=======
+import SearchResultInfiniteScroll from './components/SearchResultInfiniteScroll'
+import { searchResultPropTypes } from './constants/propTypes'
+import { getPagesArgs, getBaseMap } from './constants/SearchHelpers'
+>>>>>>> Splitting SearchResult using the Container/Component strategie
 
 import InfiniteScrollLoaderResult from './components/loaders/InfiniteScrollLoaderResult'
 import ShowMoreLoaderResult from './components/loaders/ShowMoreLoaderResult'
@@ -35,6 +42,7 @@ export default class SearchResultContainer extends Component {
     } = this.props
 
     const categories = []
+<<<<<<< HEAD
 
     if (department) {
       categories.push(department)
@@ -44,6 +52,17 @@ export default class SearchResultContainer extends Component {
       categories.push(`${department}/${category}/`)
     }
 
+=======
+
+    if (department) {
+      categories.push(department)
+    }
+
+    if (category) {
+      categories.push(`${department}/${category}/`)
+    }
+
+>>>>>>> Splitting SearchResult using the Container/Component strategie
     return {
       term,
       categories,
