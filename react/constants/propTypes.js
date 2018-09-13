dc7613f8b735d91061350353882328406460ce44
Estácio Pereira <estacio.pereira@gmail.com>
2018-09-13 08:57:21 -0300

    Add the possibility to hide the search-result facets

--- react/constants/propTypes.js
+++ react/constants/propTypes.js
@@ -133,28 +133,15 @@ export const mapType = PropTypes.string
 
 export const orderType = PropTypes.string
 
-export const hiddenFacetsSchema = {
-  /** Indicates which facets will be hidden */
-  hiddenFacets: PropTypes.shape({
-    /** Determines if the brands facets will be hidden */
-    brands: PropTypes.bool,
-    /** Determines if the categories facets will be hidden */
-    categories: PropTypes.bool,
-    /** Determines if the price range will be hidden */
-    priceRange: PropTypes.bool,
-    /** Hidden specification filters facets configuration */
-    specificationFilters: PropTypes.shape({
-      /** Determines if all the specification filters facets will be hidden */
-      hideAll: PropTypes.bool,
-      /** Array of specific hidden filters */
-      hiddenFilters: PropTypes.arrayOf(
-        PropTypes.shape({
-          /** Name of the hidden filter */
-          name: PropTypes.string,
-        })
-      ),
-    }),
-  })
+export const schemaLayoutPropTypes = {
+  /** Determines if the brands facets will be hidden */
+  hideBrands: PropTypes.bool,
+  /** Determines if the categories facets will be hidden */
+  hideCategories: PropTypes.bool,
+  /** Determines if the price range will be hidden */
+  hideRange: PropTypes.bool,
+  /** Determines if the specification filters facets will be hidden */
+  hideSpecification: PropTypes.bool,
 }
 
 export const schemaPropsTypes = {
@@ -162,7 +149,7 @@ export const schemaPropsTypes = {
   maxItemsPerPage: PropTypes.number,
   /** Product Summary's props */
   summary: PropTypes.any,
-  ...hiddenFacetsSchema,
+  ...schemaLayoutPropTypes,
 }
 
 export const searchResultContainerPropTypes = {
