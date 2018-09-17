7cf34acc49bc4136d369a772aeabedaeb204d697
Estácio Pereira <estacio.pereira@gmail.com>
2018-09-17 13:47:29 -0300

    Removed ProductSearchContextProvider state change to avoid unnecessary dependency

--- react/index.js
+++ react/index.js
@@ -11,19 +11,12 @@ import InfiniteScrollLoaderResult from './components/loaders/InfiniteScrollLoade
 import ShowMoreLoaderResult from './components/loaders/ShowMoreLoaderResult'
 import { searchResultContainerPropTypes } from './constants/propTypes'
 
-const DEFAULT_MAX_ITEMS_PER_PAGE = 10
 const PAGINATION_TYPES = ['show-more', 'infinite-scroll']
 
 /**
  * Search Result Container Component.
  */
 export default class SearchResultContainer extends Component {
-  constructor(props) {
-    super(props)
-    const { maxItemsPerPage } = props
-    props.setContextVariables({ maxItemsPerPage })
-  }
-
   state = {
     fetchMoreLoading: false,
   }
@@ -205,7 +198,6 @@ SearchResultContainer.defaultProps = {
   showMore: false,
   orderBy: SORT_OPTIONS[0].value,
   rest: '',
-  maxItemsPerPage: DEFAULT_MAX_ITEMS_PER_PAGE,
 }
 
 SearchResultContainer.uiSchema = {
@@ -223,11 +215,6 @@ SearchResultContainer.getSchema = props => {
     description: 'editor.search-result.description',
     type: 'object',
     properties: {
-      maxItemsPerPage: {
-        title: 'editor.search-result.maxItemsPerPage.title',
-        type: 'number',
-        default: DEFAULT_MAX_ITEMS_PER_PAGE,
-      },
       hiddenFacets: {
         title: 'editor.search-result.hiddenFacets',
         type: 'object',
