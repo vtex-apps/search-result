dfe5fd03c9fa45e47c4585ab3afae073a1d7d6bb
Rerisson Daniel <rerisson.matos@splab.ufcg.edu.br>
2018-11-07 12:14:37 -0300

    Fix line break

--- react/components/FooterButton.js
+++ react/components/FooterButton.js
@@ -5,8 +5,7 @@ export default function FooterButton({ children, tag, ...props }) {
   const Tag = tag || 'button'
 
   return (
-    <Tag {...props} className="vtex-footer-button no-underline pointer t-action--small bg-base--inverted br1 c-on-base--inverted 
-      ba b--muted-5 pv2 ph7 hover-bg-muted-5 hover-c-action-primary">
+    <Tag {...props} className="vtex-footer-button no-underline pointer t-action--small bg-base--inverted br1 c-on-base--inverted ba b--muted-5 pv2 ph7 hover-bg-muted-5 hover-c-action-primary">
       {children}
     </Tag>
   )
