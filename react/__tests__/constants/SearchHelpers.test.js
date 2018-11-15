3345f592476a051a9c43988df9f412ba6143c775
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-15 17:36:41 -0300

    Update tests for specification filters

--- react/__tests__/constants/SearchHelpers.test.js
+++ react/__tests__/constants/SearchHelpers.test.js
@@ -10,9 +10,9 @@ import {
 describe('getSpecificationFilterFromLink', () => {
   it('should return the only specification in link', () => {
     const link = '/eletronicos/smartphones/Android 7.0?map=c,c,specificationFilter_20'
-    const map = ['c', 'c']
+    const slug = 'Android 7.0'
 
-    const filterMap = getSpecificationFilterFromLink(link, map)
+    const filterMap = getSpecificationFilterFromLink(link, slug)
 
     expect(filterMap).toBe('specificationFilter_20')
   })
@@ -20,9 +20,9 @@ describe('getSpecificationFilterFromLink', () => {
   it('should return the first non-equal specification', () => {
     const link =
       '/eletronicos/smartphones/Android 7.0/3 GB?map=c,c,specificationFilter_20,specificationFilter_21'
-    const map = ['c', 'c', 'specificationFilter_20']
+    const slug = '3 GB'
 
-    const filterMap = getSpecificationFilterFromLink(link, map)
+    const filterMap = getSpecificationFilterFromLink(link, slug)
 
     expect(filterMap).toBe('specificationFilter_21')
   })
@@ -30,9 +30,9 @@ describe('getSpecificationFilterFromLink', () => {
   it('should return the duplicated specification', () => {
     const link =
       '/eletronicos/smartphones/Android 7.0/Android 7.1?map=c,c,specificationFilter_20,specificationFilter_20'
-    const map = ['c', 'c', 'specificationFilter_20']
+    const slug = 'Android 7.1'
 
-    const filterMap = getSpecificationFilterFromLink(link, map)
+    const filterMap = getSpecificationFilterFromLink(link, slug)
 
     expect(filterMap).toBe('specificationFilter_20')
   })
@@ -40,20 +40,20 @@ describe('getSpecificationFilterFromLink', () => {
   it('should ignore the order of the elements', () => {
     const link =
       '/eletronicos/smartphones/3 GB/Android 7.1?map=c,c,specificationFilter_20,specificationFilter_21,specificationFilter_22'
-    const map = ['c', 'c', 'specificationFilter_20', 'specificationFilter_22']
+    const slug = 'Android 7.1'
 
-    const filterMap = getSpecificationFilterFromLink(link, map)
+    const filterMap = getSpecificationFilterFromLink(link, slug)
 
     expect(filterMap).toBe('specificationFilter_21')
   })
 
   it('should bail out if can\'t match the params', () => {
     const link = '/eletronics/smartphones/android 7.1?map=c,c,specificationFilter_20'
-    const map = ['c', 'c', 'specificationFilter_21']
+    const slug = 'Android 7.1'
 
-    const filterMap = getSpecificationFilterFromLink(link, map)
+    const filterMap = getSpecificationFilterFromLink(link, slug)
 
-    expect(filterMap).toBe('specificationFilter_20')
+    expect(filterMap).toBe(undefined)
   })
 })
 
