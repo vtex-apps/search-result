f35b0feb0d98691080ea9ca38e62863039f6c2f6
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2018-11-22 15:20:28 -0300

    Update icons with data from iconPack

--- react/images/InlineGrid.js
+++ react/images/InlineGrid.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function InlineGrid({ active }) {
-  const color = active ? '#828282' : '#ececec'
-
+/**
+ * InlineGrid icon component in svg
+ */
+const InlineGrid = ({ size, fillColor }) => {
   return (
-    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect y="14" width="5" height="5" fill={color} />
-      <rect y="7" width="5" height="5" fill={color} />
-      <rect width="5" height="5" fill={color} />
-      <rect x="7" width="12" height="2" fill={color} />
-      <rect x="7" y="3" width="12" height="2" fill={color} />
-      <rect x="7" y="7" width="12" height="2" fill={color} />
-      <rect x="7" y="10" width="12" height="2" fill={color} />
-      <rect x="7" y="14" width="12" height="2" fill={color} />
-      <rect x="7" y="17" width="12" height="2" fill={color} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width={size}
+      height={size}
+      viewBox="0 0 16 16"
+      fill="none"
+      color={fillColor}
+    >
+      <use href="#list-grid" xlinkHref="#list-grid" />
     </svg>
   )
 }
 
 InlineGrid.propTypes = {
-  /** Whether the icon is active */
-  active: PropTypes.bool,
+  /* Percentage size of the icon */
+  size: PropTypes.number,
+  /* Fill color for the icon */
+  fillColor: PropTypes.string,
 }
+
+InlineGrid.defaultProps = {
+  size: 20,
+  fillColor: '#3F3F40',
+}
+
+export default InlineGrid
