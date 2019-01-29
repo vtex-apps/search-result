86dd652f0b9ce67b3d672adc0489d7666516256d
Bruno Dias <bruno.dias@vtex.com.br>
2019-01-29 12:59:17 -0300

    Fix review issues

--- react/TotalProducts.js
+++ react/TotalProducts.js
@@ -1,21 +1,16 @@
 import { FormattedMessage } from 'react-intl'
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import searchResult from './searchResult.css'
 
 const TotalProducts = ({ recordsFiltered }) => {
   return (
-    <Fragment>
       <div className={`${searchResult.totalProducts} pv5 bn-ns bt-s b--muted-4 tc-s tl`}>
-        <FormattedMessage
-          id="search.total-products"
-          values={{ recordsFiltered }}
-        >
+        <FormattedMessage id="search.total-products" values={{ recordsFiltered }}>
           {txt => <span className="ph4 c-muted-2">{txt}</span>}
         </FormattedMessage>
       </div>
-    </Fragment>
   )
 }
 
