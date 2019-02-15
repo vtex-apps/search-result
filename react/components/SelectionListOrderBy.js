6d3a48f785c8141e0f1d412652af11de410eb431
Juan Almeida <juan.satyro@gmail.com>
2019-02-15 12:32:30 -0200

    Change SelectionListOrderBy to functional and use hooks

--- react/components/SelectionListOrderBy.js
+++ react/components/SelectionListOrderBy.js
@@ -1,44 +1,28 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 import classNames from 'classnames'
 import { find, propEq } from 'ramda'
 
-import { withRuntimeContext, Link } from 'vtex.render-runtime'
+import { useRuntime, Link } from 'vtex.render-runtime'
 import { IconCaret } from 'vtex.dreamstore-icons'
 
 import searchResult from '../searchResult.css'
 
-class SelectionListOrderBy extends Component {
-  state = {
-    showDropdown: false,
-  }
+const SelectionListOrderBy = ({
+  orderBy,
+  getLinkProps,
+  options,
+  intl,
+}) => {
+  const [showDropdown, setShowDropdown] = useState(false)
 
-  handleDropdownBtClick = e => {
+  const handleDropdownBtClick = e => {
     e.preventDefault()
-    this.setState({ showDropdown: !this.state.showDropdown })
+    setShowDropdown(!showDropdown)
   }
 
-  handleOutsideClick = e => {
-    this.setState({ showDropdown: false })
-  }
-
-  static propTypes = {
-    mobile: PropTypes.bool,
-    orderBy: PropTypes.string,
-    getLinkProps: PropTypes.func,
-    options: PropTypes.arrayOf(PropTypes.shape({
-      label: PropTypes.string,
-      value: PropTypes.string,
-    })),
-    intl: intlShape,
-    runtime: PropTypes.shape({
-      navigate: PropTypes.func,
-    }),
-  }
-
-  renderOptions = () => {
-    const { options, getLinkProps } = this.props
+  const renderOptions = () => {
     return options.map(option => {
       const linkProps = getLinkProps({ ordenation: option.value })
       return (
@@ -55,46 +39,53 @@ class SelectionListOrderBy extends Component {
     })
   }
 
-  getOptionTitle = option => {
-    const { options } = this.props
+  const getOptionTitle = option => {
     return find(propEq('value', option), options).label
   }
 
-  render() {
-    const { orderBy, mobile, intl } = this.props
-    const { showDropdown } = this.state
-    const btClass = classNames('ph3 pv5 mv0 pointer flex justify-center items-center bg-base c-on-base t-action--small ml-auto bt br bl bb-0 br2 br--top bw1 w-100',
-      {
-        'b--muted-4 shadow-1': showDropdown && mobile,
-        'b--transparent pl1': !showDropdown,
-      }
-    )
+  const { hints: { mobile } } = useRuntime()
 
-    const contentClass = classNames('z-1 absolute bg-base shadow-5 f5 w-100 b--muted-4 br2 ba bw1 br--bottom',
-      {
-        'db': showDropdown,
-        'dn': !showDropdown,
-      }
-    )
+  const btClass = classNames('ph3 pv5 mv0 pointer flex justify-center items-center bg-base c-on-base t-action--small ml-auto bt br bl bb-0 br2 br--top bw1 w-100',
+    {
+      'b--muted-4 shadow-1': showDropdown && mobile,
+      'b--transparent pl1': !showDropdown,
+    }
+  )
 
-    const dropdownSort = classNames(searchResult.dropdownSort, 'relative pt1 dib', {
-      'flex-auto justify-center w-100': mobile,
-    })
+  const contentClass = classNames('z-1 absolute bg-base shadow-5 f5 w-100 b--muted-4 br2 ba bw1 br--bottom',
+    {
+      'db': showDropdown,
+      'dn': !showDropdown,
+    }
+  )
 
-    return (
-      <div className={dropdownSort}>
-        <button onClick={this.handleDropdownBtClick} className={btClass}>
-          <span className={`${searchResult.filterPopupTitle} c-on-base t-action--small ml-auto`}>{this.getOptionTitle(orderBy)} </span>
-          <span className={`${searchResult.filterPopupArrowIcon} pt1 ml-auto`}>
-            <IconCaret orientation="down" size={10} />
-          </span>
-        </button>
-        <div className={contentClass}>
-          {this.renderOptions()}
-        </div>
+  const dropdownSort = classNames(searchResult.dropdownSort, 'relative pt1 dib', {
+    'flex-auto justify-center w-100': mobile,
+  })
+
+  return (
+    <div className={dropdownSort}>
+      <button onClick={handleDropdownBtClick} className={btClass}>
+        <span className={`${searchResult.filterPopupTitle} c-on-base t-action--small ml-auto`}>{getOptionTitle(orderBy)} </span>
+        <span className={`${searchResult.filterPopupArrowIcon} pt1 ml-auto`}>
+          <IconCaret orientation="down" size={10} />
+        </span>
+      </button>
+      <div className={contentClass}>
+        {renderOptions()}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default injectIntl(withRuntimeContext(SelectionListOrderBy))
+SelectionListOrderBy.propTypes = {
+  orderBy: PropTypes.string,
+  getLinkProps: PropTypes.func,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+  })),
+  intl: intlShape,
+}
+
+export default injectIntl(SelectionListOrderBy)
