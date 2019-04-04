d6679d910e0fddd66126b9061c57b5cf8058bccb
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-04 15:21:03 -0300

    Fix errors on filter sidebar and accordion

--- react/components/AccordionFilterContainer.js
+++ react/components/AccordionFilterContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 import classNames from 'classnames'
@@ -8,106 +8,94 @@ import AccordionFilterItem from './AccordionFilterItem'
 
 import searchResult from '../searchResult.css'
 
-class AccordionFilterContainer extends Component {
-  static propTypes = {
-    /** Current available filters */
-    filters: PropTypes.arrayOf(PropTypes.object),
-    /** Intl instance */
-    intl: intlShape,
-    /** Filters mapped for checkbox */
-    filtersChecks: PropTypes.object,
-    /** Checkbox hit callback function */
-    onFilterCheck: PropTypes.func,
-    /** Filters selected previously */
-    selectedFilters: PropTypes.array,
-    isOptionSelected: PropTypes.func.isRequired,
-  }
+const AccordionFilterContainer = ({
+  filters,
+  intl,
+  onFilterCheck,
+  isOptionSelected,
+}) => {
+  const [openItem, setOpenItem] = useState(null)
 
-  state = {
-    openedItem: null,
-  }
-
-  handleItemSelected = onClose => e => {
-    onClose(e)
-
-    this.setState({
-      openedItem: null,
-    })
-  }
-
-  handleOpen = id => e => {
+  const handleOpen = id => e => {
     e.preventDefault()
 
-    if (this.state.openedItem === id) {
-      this.setState({
-        openedItem: null,
-      })
+    if (openItem === id) {
+      setOpenItem(null)
     } else {
-      this.setState({
-        openedItem: id,
-      })
+      setOpenItem(id)
     }
   }
 
-  render() {
-    const { filters, intl, onFilterCheck, isOptionSelected } = this.props
-    const { openedItem } = this.state
+  const nonEmptyFilters = filters.filter(spec => spec.facets.length > 0)
 
-    const nonEmptyFilters = filters.filter(spec => spec.facets.length > 0)
-    return (
-      <div className={`${searchResult.accordionFilter} h-100`}>
+  return (
+    <div className={`${searchResult.accordionFilter} h-100`}>
+      <div
+        className={`${
+          searchResult.filterAccordionBreadcrumbs
+        } pointer flex flex-row items-center pa5 bg-base w-100 z-max bb b--muted-4`}
+      >
         <div
-          className={`${
-            searchResult.filterAccordionBreadcrumbs
-          } pointer flex flex-row items-center pa5 bg-base w-100 z-max bb b--muted-4`}
+          role="button"
+          tabIndex={0}
+          className="pv4 flex items-center"
+          onClick={() => setOpenItem(null)}
+          onKeyDown={e => e.key === ' ' && setOpenItem(null)}
         >
           <div
-            role="button"
-            tabIndex={0}
-            className="pv4 flex items-center"
-            onClick={() => this.setState({ openedItem: null })}
+            className={classNames('t-heading-4', {
+              'c-muted-2': openItem,
+              'c-on-base': !openItem,
+            })}
           >
-            <div
-              className={classNames('t-heading-4', {
-                'c-muted-2': !!openedItem,
-                'c-on-base': !openedItem,
-              })}
-            >
-              {intl.formatMessage({
-                id: 'search-result.filter-breadcrumbs.primary',
-              })}
+            {intl.formatMessage({
+              id: 'search-result.filter-breadcrumbs.primary',
+            })}
+          </div>
+        </div>
+        {openItem && (
+          <div className="pa4 flex items-center">
+            <IconCaret orientation="right" size={13} />
+            <div className="pl3 t-heading-4 c-on-base">
+              {intl.formatMessage({ id: openItem })}
             </div>
           </div>
-          {openedItem && (
-            <div className="pa4 flex items-center">
-              <IconCaret orientation="right" size={13} />
-              <div className="pl3 t-heading-4 c-on-base">
-                {intl.formatMessage({ id: openedItem })}
-              </div>
-            </div>
-          )}
-        </div>
-
-        {nonEmptyFilters.map(filter => {
-          const { title, options } = filter
-          const isOpen = openedItem === filter.title
-
-          return (
-            <AccordionFilterItem
-              key={filter.title}
-              title={title}
-              options={options}
-              isOptionSelected={isOptionSelected}
-              open={isOpen}
-              onFilterCheck={onFilterCheck}
-              show={!openedItem || isOpen}
-              onOpen={this.handleOpen(filter.title)}
-            />
-          )
-        })}
+        )}
       </div>
-    )
-  }
+
+      {nonEmptyFilters.map(filter => {
+        const { title, facets } = filter
+        const isOpen = openItem === filter.title
+
+        return (
+          <AccordionFilterItem
+            key={filter.title}
+            title={title}
+            facets={facets}
+            isOptionSelected={isOptionSelected}
+            open={isOpen}
+            onFilterCheck={onFilterCheck}
+            show={!openItem || isOpen}
+            onOpen={handleOpen(filter.title)}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+AccordionFilterContainer.propTypes = {
+  /** Current available filters */
+  filters: PropTypes.arrayOf(PropTypes.object),
+  /** Intl instance */
+  intl: intlShape,
+  /** Filters mapped for checkbox */
+  filtersChecks: PropTypes.object,
+  /** Checkbox hit callback function */
+  onFilterCheck: PropTypes.func,
+  /** Filters selected previously */
+  selectedFilters: PropTypes.array,
+  isOptionSelected: PropTypes.func.isRequired,
 }
 
 export default injectIntl(AccordionFilterContainer)
