3cad7b6df9a1ce779b339f9c61d8ab08b87ac928
Sahan Jayawardana <sahan@clouda.io>
2019-04-11 11:47:16 +0530

    [CP] Replace class with a functional component

--- react/CategoryPanel.js
+++ react/CategoryPanel.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { Link } from 'vtex.render-runtime'
@@ -6,86 +6,78 @@ import { facetOptionShape } from './constants/propTypes'
 import searchResult from './searchResult.css'
 import CategoriesHighlights from 'vtex.store-components/CategoriesHighlights'
 
-class CategoryPanel extends Component {
-  static propTypes = {
-    /** Category tree */
-    tree: PropTypes.arrayOf(facetOptionShape),
-    /** Number of category filters to be shown */
-    noOfCategories: PropTypes.number,
-    /** Number of categories per row */
-    quantityOfItemsPerRow: PropTypes.number,
+const renderCategoryShelf = (category, noOfChildren, quantityOfItemsPerRow) => {
+  const categoriesHighlighted = category.Children.slice(0, noOfChildren).map(
+    child => ({
+      id: child.Id,
+      name: child.Name,
+      to: child.Link,
+    })
+  )
+  const headerClasses = classNames(
+    searchResult.categoryPanelHeaderRow,
+    'flex flex-row flex-wrap items-stretch pa3 bn mh4-ns'
+  )
+  const itemClasses = classNames(
+    searchResult.categoryPanelItemRow,
+    'flex flex-row flex-wrap items-stretch pa3 bn mh4-ns'
+  )
+  let i = 0
+  const items = []
+  for (i = 0; i < categoriesHighlighted.length; i += quantityOfItemsPerRow) {
+    items.push(categoriesHighlighted.slice(i, i + quantityOfItemsPerRow))
   }
+  return (
+    <Fragment key={`parent-fragment-${category.Id}`}>
+      <h3 className={`t-heading-3 ${headerClasses}`}>
+        <Link to={category.Link}>{category.Name}</Link>
+      </h3>
+      {items.length > 0 &&
+        items.map((item, index) => (
+          <div key={`category-row-${index}`} className={itemClasses}>
+            <CategoriesHighlights
+              categoriesHighlighted={item}
+              showCategoriesHighlighted={true}
+              quantityOfItems={quantityOfItemsPerRow}
+            />
+          </div>
+        ))}
+      {category.Children.map(child => {
+        if (child.Children && child.Children.length > 0) {
+          return renderCategoryShelf(child, noOfChildren, quantityOfItemsPerRow)
+        }
+      })}
+    </Fragment>
+  )
+}
 
-  static defaultProps = {
-    tree: [],
-    noOfCategories: 1000,
-    quantityOfItemsPerRow: 4,
-  }
+const CategoryPanel = ({ tree, noOfCategories, quantityOfItemsPerRow }) => {
+  return (
+    <div>
+      {tree.map(category => {
+        return renderCategoryShelf(
+          category,
+          noOfCategories,
+          quantityOfItemsPerRow
+        )
+      })}
+    </div>
+  )
+}
 
-  renderCategoryShelf = (
-    category,
-    noOfChildren,
-    quantityOfItemsPerRow
-  ) => {
-    const categoriesHighlighted = category.Children.slice(0, noOfChildren).map(
-      child => ({
-        id: child.Id,
-        name: child.Name,
-        to: child.Link,
-      })
-    )
-    const headerClasses = classNames(
-      searchResult.categoryPanelHeaderRow,
-      'flex flex-row flex-wrap items-stretch pa3 bn mh4-ns'
-    )
-    const itemClasses = classNames(
-      searchResult.categoryPanelItemRow,
-      'flex flex-row flex-wrap items-stretch pa3 bn mh4-ns'
-    )
-    let i = 0
-    const items = []
-    for (i = 0; i < categoriesHighlighted.length; i += quantityOfItemsPerRow) {
-      items.push(categoriesHighlighted.slice(i, i + quantityOfItemsPerRow))
-    }
-    return (
-      <Fragment key={`parent-fragment-${category.Id}`}>
-        <h3 className={`t-heading-3 ${headerClasses}`}>
-          <Link to={category.Link}>{category.Name}</Link>
-        </h3>
-        {items.length > 0 &&
-          items.map((item, index) => (
-            <div key={`category-row-${index}`} className={itemClasses}>
-              <CategoriesHighlights
-                categoriesHighlighted={item}
-                showCategoriesHighlighted={true}
-                quantityOfItems={quantityOfItemsPerRow}
-              />
-            </div>
-          ))}
-        {category.Children.map(child => {
-          if (child.Children && child.Children.length > 0) {
-            return this.renderCategoryShelf(child, noOfChildren, quantityOfItemsPerRow)
-          }
-        })}
-      </Fragment>
-    )
-  }
+CategoryPanel.propTypes = {
+  /** Category tree */
+  tree: PropTypes.arrayOf(facetOptionShape),
+  /** Number of category filters to be shown */
+  noOfCategories: PropTypes.number,
+  /** Number of categories per row */
+  quantityOfItemsPerRow: PropTypes.number,
+}
 
-  render() {
-    const { tree, noOfCategories, quantityOfItemsPerRow } = this.props
-
-    return (
-      <div>
-        {tree.map(category => {
-          return this.renderCategoryShelf(
-            category,
-            noOfCategories,
-            quantityOfItemsPerRow
-          )
-        })}
-      </div>
-    )
-  }
+CategoryPanel.defaultProps = {
+  tree: [],
+  noOfCategories: 1000,
+  quantityOfItemsPerRow: 4,
 }
 
 export default CategoryPanel
