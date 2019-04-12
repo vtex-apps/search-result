f02aaba6efad2ceb718db51fa50bfbdb14c0b4a3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-12 10:28:30 -0300

    Remove stale tests

--- react/__tests__/FilterNavigator.test.js
+++ react/__tests__/FilterNavigator.test.js
@@ -2,35 +2,21 @@
 import React from 'react'
 import { render } from '@vtex/test-tools/react'
 
-import categoriesTree, { numberOfFilters } from 'categoriesTree'
+import categoriesTree from 'categoriesTree'
 
 import FilterNavigator from '../FilterNavigator'
 
 describe('<FilterNavigator />', () => {
   const renderComponent = customProps => {
-    const getLinkProps = jest.fn().mockImplementation(pageNumber => {
-      return {
-        page: `mockedPage${pageNumber}`,
-        params: 'mockedParams',
-      }
-    })
     const props = {
-      getLinkProps,
       map: 'c',
       rest: '',
       tree: categoriesTree,
       ...customProps,
     }
 
-    return { ...render(<FilterNavigator {...props} />), getLinkProps }
+    return render(<FilterNavigator {...props} />)
   }
-
-  it('should call getLinkProps on render', () => {
-    const { getLinkProps } = renderComponent()
-
-    // Expect getLinkProps has been called
-    expect(getLinkProps).toBeCalledTimes(numberOfFilters)
-  })
 
   it('should match snapshot with all', () => {
     const { asFragment } = renderComponent()
