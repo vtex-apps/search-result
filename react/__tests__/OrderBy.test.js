bd46a965fe8de4766fdc4bc7785b368808397234
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-12 10:28:30 -0300

    Remove stale tests

--- react/__tests__/OrderBy.test.js
+++ react/__tests__/OrderBy.test.js
@@ -8,20 +8,13 @@ import { setMobileState } from 'vtex.render-runtime'
 describe('<OrderBy />', () => {
   const renderComponent = customProps => mobile => {
     setMobileState(mobile)
-    const getLinkProps = jest.fn().mockImplementation(pageNumber => {
-      return {
-        page: `mockedPage${pageNumber}`,
-        params: 'mockedParams',
-      }
-    })
 
     const props = {
       orderBy: 'OrderByTopSaleDESC',
-      getLinkProps,
       ...customProps,
     }
 
-    return { ...render(<OrderBy {...props} />), getLinkProps }
+    return render(<OrderBy {...props} />)
   }
 
   it('should shown dropdown box on mobile mode', () => {
@@ -39,15 +32,6 @@ describe('<OrderBy />', () => {
 
     // Expect Dropdown container be rendered without display none
     expect(dropdownContainer).toBeNull()
-  })
-
-  it('should call getLinkProps', () => {
-    const { container, getLinkProps } = renderComponent()(true)
-
-    const button = container.querySelector('button')
-    fireEvent.click(button)
-    // Expect Dropdown container be rendered without display none
-    expect(getLinkProps).toBeCalled()
   })
 
   it('should match snapshot in web mod', () => {
