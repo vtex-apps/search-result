c3bbd096b79719027cffbdf5a61207045b3bd577
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-25 16:13:43 -0300

    Add default value for query and convert to function component

--- react/components/LocalQuery.js
+++ react/components/LocalQuery.js
@@ -1,79 +1,75 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Query } from 'react-apollo'
-import { withRuntimeContext } from 'vtex.render-runtime'
-import { Queries } from 'vtex.store-resources'
+import { useRuntime } from 'vtex.render-runtime'
+import { productSearch } from 'vtex.store-resources/Queries'
 
 import { SORT_OPTIONS } from '../OrderBy'
 
 const DEFAULT_PAGE = 1
 
-class LocalQuery extends Component {
-  static defaultProps = {
-    orderByField: SORT_OPTIONS[0].value,
-  }
+const LocalQuery = props => {
+  const {
+    maxItemsPerPage,
+    queryField,
+    mapField,
+    orderByField = SORT_OPTIONS[0].value,
+    query: {
+      order: orderBy = orderByField,
+      page: pageQuery,
+      priceRange,
+      map = mapField,
+    } = {},
+    render,
+  } = props
 
-  render() {
-    const {
-      maxItemsPerPage,
-      queryField,
-      mapField,
-      orderByField,
-      query: {
-        order: orderBy = orderByField,
-        page: pageQuery,
+  const { page: runtimePage } = useRuntime()
+
+  const page = pageQuery ? parseInt(pageQuery) : DEFAULT_PAGE
+  const from = (page - 1) * maxItemsPerPage
+  const to = from + maxItemsPerPage - 1
+
+  return (
+    <Query
+      query={productSearch}
+      variables={{
+        query: queryField,
+        map,
+        orderBy,
         priceRange,
-        map = mapField,
-      },
-      runtime: { page: runtimePage },
-    } = this.props
+        from,
+        to,
+        withFacets: !!(
+          map &&
+          map.length > 0 &&
+          queryField &&
+          queryField.length > 0
+        ),
+      }}
+      notifyOnNetworkStatusChange
+      partialRefetch
+    >
+      {searchQueryProps => {
+        const { data } = searchQueryProps
+        const { search } = data || {}
 
-    const page = pageQuery ? parseInt(pageQuery) : DEFAULT_PAGE
-    const from = (page - 1) * maxItemsPerPage
-    const to = from + maxItemsPerPage - 1
-
-    return (
-      <Query
-        query={Queries.search}
-        variables={{
-          query: queryField,
+        return render({
+          ...props,
+          searchQuery: {
+            ...searchQueryProps,
+            ...search,
+          },
+          searchContext: runtimePage,
+          pagesPath: runtimePage,
           map,
           orderBy,
           priceRange,
+          page,
           from,
           to,
-          withFacets: !!(
-            map &&
-            map.length > 0 &&
-            queryField &&
-            queryField.length > 0
-          ),
-        }}
-        notifyOnNetworkStatusChange
-        partialRefetch
-      >
-        {searchQueryProps => {
-          const { data } = searchQueryProps
-          const { search } = data || {}
-
-          return this.props.render({
-            ...this.props,
-            searchQuery: {
-              ...searchQueryProps,
-              ...search,
-            },
-            searchContext: runtimePage,
-            pagesPath: runtimePage,
-            map,
-            orderBy,
-            priceRange,
-            page,
-            from,
-            to,
-          })
-        }}
-      </Query>
-    )
-  }
+        })
+      }}
+    </Query>
+  )
 }
 
-export default withRuntimeContext(LocalQuery)
+export default LocalQuery
