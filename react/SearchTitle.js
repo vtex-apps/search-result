ec6f1128615dc2f58760684d94ee600bbce8baec
Lucas Bebber <lucasbbebber@gmail.com>
2019-05-07 23:05:29 -0300

    Makes SearchTitle get product cluster name if it maps to the title

--- react/SearchTitle.js
+++ react/SearchTitle.js
@@ -1,11 +1,51 @@
 import classNames from 'classnames'
 import React from 'react'
+import { zip } from 'ramda'
 
 import styles from './searchResult.css'
 
-const SearchTitle = ({ params }) => {
-  const title =
-    params.term || params.subcategory || params.category || params.department
+const getClusterById = (products, id) => {
+  if (!products) {
+    return
+  }
+
+  for (const product of products) {
+    for (const cluster of product.productClusters) {
+      if (cluster.id === id) {
+        return cluster.name
+      }
+    }
+  }
+}
+
+const SearchTitle = ({ params, map, products }) => {
+  // Terrible hotfix for product cluster name below!!!
+  // Should be updated with a better version
+
+  let title = null
+
+  // matches title "sources" with its respective map,
+  // and gets the last valid one
+  const titleSources = zip([
+    params.department,
+    params.category,
+    params.subcategory,
+    params.term,
+  ], map.split(',')).reverse()
+  const [titleValue, titleMap] = titleSources.find(([param]) => !!param)
+
+  // if the title maps to productClusterIds, gets the cluster name
+  // from the product list (which hopefully at least one
+  // product will have listed on its productClusters key)
+  if (titleMap === 'productClusterIds') {
+    const productCluster = getClusterById(products, titleValue)
+
+    if (productCluster) {
+      title = productCluster
+    }
+  } else {
+    title = titleValue
+  }
 
   if (!title) {
     return null
