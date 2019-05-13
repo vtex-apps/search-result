0f4b2034eee03a0b403dedeacef6796eb48d5015
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2019-05-13 16:20:51 -0300

    Clean subcategory filter based on category

--- react/components/FacetItem.js
+++ react/components/FacetItem.js
@@ -22,28 +22,49 @@ const FacetItem = ({ facet }) => {
   const { query, map } = useContext(QueryContext)
   const { showFacetQuantity } = useContext(SettingsContext)
 
+  const getFacetIndex = facet => {
+    return query
+      .toLowerCase()
+      .split('/')
+      .map(decodeURIComponent)
+      .findIndex(
+        value => value === decodeURIComponent(facet.value).toLowerCase()
+      )
+  }
+
+  const removeChildrenSelected = (facet, urlParams) => {
+    let newQuery = query
+    for (const child of facet.children) {
+      if (child.selected) {
+        if (child.children) {
+          for (const c of child.children) {
+            newQuery = removeChildrenSelected(c, urlParams)
+          }
+        }
+        const childIndex = getFacetIndex(child)
+        newQuery = removeElementAtIndex(query, childIndex, '/')
+        urlParams.set('map', removeElementAtIndex(map, childIndex, ','))
+      }
+    }
+    return newQuery
+  }
+
   const handleChange = () => {
     const urlParams = new URLSearchParams(window.location.search)
 
     if (facet.selected) {
-      const facetIndex = query
-        .toLowerCase()
-        .split('/')
-        .map(decodeURIComponent)
-        .findIndex(
-          value => value === decodeURIComponent(facet.value).toLowerCase()
-        )
-
+      const facetIndex = getFacetIndex(facet)
+      let newQuery = removeChildrenSelected(facet, urlParams)
       urlParams.set('map', removeElementAtIndex(map, facetIndex, ','))
 
       navigate({
-        to: `/${removeElementAtIndex(query, facetIndex, '/')}`,
+        to: `/${removeElementAtIndex(newQuery, facetIndex, '/')}`,
         query: urlParams.toString(),
         scrollOptions,
       })
       return
     }
-
+    
     urlParams.set('map', `${map},${facet.map}`)
 
     navigate({
