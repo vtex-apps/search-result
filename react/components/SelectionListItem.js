976ca376bc3761c9dcd96de5d182b75836803c65
iaron <iaron.araujo@vtex.com.br>
2019-08-01 13:48:18 -0300

    Improved `sortBy` dropdown design

--- react/components/SelectionListItem.js
+++ react/components/SelectionListItem.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useRuntime } from 'vtex.render-runtime'
+import classNames from 'classnames'
+import searchResult from '../searchResult.css'
 
-const SelectionListItem = ({ option, onItemClick }) => {
+const SelectionListItem = ({ option, onItemClick, selectedSpotlight }) => {
   const { setQuery } = useRuntime()
 
   const handleOptionClick = () => {
@@ -9,9 +11,17 @@ const SelectionListItem = ({ option, onItemClick }) => {
     setQuery({ order: option.value })
   }
 
+  const spotlight = selectedSpotlight
+    ? 'bg-light-gray'
+    : 'hover-bg-muted-5 bg-base'
+
   return (
     <button
-      className="bg-base c-on-base f5 ml-auto db no-underline pointer tl bn pv4 ph5 hover-bg-muted-4 w-100"
+      className={classNames(
+        searchResult.orderByOptionItem,
+        spotlight,
+        ' c-on-base f5 ml-auto db no-underline pointer tl bn pv4 ph5 w-100 right-0-ns'
+      )}
       key={option.value}
       onClick={handleOptionClick}
     >
