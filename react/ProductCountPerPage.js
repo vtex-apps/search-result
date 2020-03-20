077a48fc28ede39c429b2ac2cdc9e3673f8b0fff
marcosewbank <ewbankmarcos@gmail.com>
2020-03-20 15:36:41 -0300

    Progressbar added at productCountPerPage component

--- react/ProductCountPerPage.js
+++ react/ProductCountPerPage.js
@@ -5,10 +5,19 @@ import { FormattedMessage } from 'react-intl'
 import { useSearchPage } from 'vtex.search-page-context/SearchPageContext'
 import { useCssHandles } from 'vtex.css-handles'
 
-const CSS_HANDLES = ['showingProducts', 'showingProductsCount']
+const CSS_HANDLES = [
+  'showingProducts',
+  'showingProductsCount',
+  'showingProductsContainer',
+  'showingAllProductsCount',
+  'progressBar',
+  'progressBarFiller',
+]
 
-const ProductCountPerPage = () => {
+const ProductCountPerPage = props => {
+  const { showProductCountProgressBar = 'hidden' } = props
   const { searchQuery } = useSearchPage()
+
   const handles = useCssHandles(CSS_HANDLES)
   const products =
     path(['data', 'productSearch', 'products'], searchQuery) || []
@@ -17,29 +26,62 @@ const ProductCountPerPage = () => {
     searchQuery
   )
 
+  const productsLoadedPercentage = Math.round(
+    (100 * products.length) / recordsFiltered
+  )
   if (products.length === 0) {
     return null
   }
 
+  const showingProducts =
+    productsLoadedPercentage !== 100
+      ? {
+          showingProductsId: 'store/search-result.showing-products',
+          showingProductsCountId: 'store/search-result.showing-products-count',
+        }
+      : {
+          showingProductsId: 'store/search-result.showing-all-products',
+          showingProductsCountId:
+            'store/search-result.showing-all-products-count',
+        }
+
   return (
-    <div className={`${handles.showingProducts} tc t-small pt3 c-muted-2`}>
-      <FormattedMessage
-        id="store/search-result.showing-products"
-        tagName="span"
-        values={{
-          value: (
-            <span className={`${handles.showingProductsCount} b`}>
-              <FormattedMessage
-                id="store/search-result.showing-products-count"
-                values={{
-                  productsLoaded: products.length,
-                  total: recordsFiltered,
-                }}
-              />
-            </span>
-          ),
-        }}
-      />
+    <div
+      className={`${handles.showingProductsContainer} flex flex-column justify-center items-center`}
+    >
+      <div
+        className={`
+        ${handles.showingProducts} tc t-small pt3 c-muted-2
+      `}
+      >
+        <FormattedMessage
+          id={showingProducts.showingProductsId}
+          tagName="span"
+          values={{
+            value: (
+              <span className={`${handles.showingProductsCount} b`}>
+                <FormattedMessage
+                  id={showingProducts.showingProductsCountId}
+                  values={{
+                    productsLoaded: products.length,
+                    total: recordsFiltered,
+                  }}
+                />
+              </span>
+            ),
+          }}
+        />
+      </div>
+      {showProductCountProgressBar === 'show' && productsLoadedPercentage && (
+        <div
+          className={`${handles.progressBar} relative flex bg-action-secondary items-center mt4 br-pill w-50 h1`}
+        >
+          <span
+            className={`${handles.progressBarFiller} absolute br-pill bg-action-primary h1`}
+            style={{ width: `${productsLoadedPercentage}%` }}
+          />
+        </div>
+      )}
     </div>
   )
 }
