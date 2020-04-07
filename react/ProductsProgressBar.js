7704eae5c4e30e3c16f1e3654f51e389927c0b99
Breno Calazans <brenocalazans@gmail.com>
2020-04-07 15:24:50 -0300

    Remove unecessary flex

--- react/ProductsProgressBar.js
+++ react/ProductsProgressBar.js
@@ -24,11 +24,9 @@ const ProductsProgressBar = () => {
   }
 
   return (
-    <div
-      className={`${handles.progressBarContainer} flex justify-center items-center`}
-    >
+    <div className={handles.progressBarContainer}>
       <div
-        className={`${handles.progressBar} relative flex bg-action-secondary items-center mv4 br-pill w-50 w-30-m w-25-l h1`}
+        className={`${handles.progressBar} relative flex bg-action-secondary center mv4 br-pill w-50 w-30-m w-25-l h1`}
       >
         <span
           className={`${handles.progressBarFiller} absolute br-pill bg-action-primary h1`}
