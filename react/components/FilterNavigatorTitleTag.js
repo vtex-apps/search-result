8acd322d03ec2b0ff44e8bd6057812b224d05ef3
Ronaldo Caetano <ronaldogodoi2@gmail.com>
2020-08-01 13:44:41 -0300

    adding CustiomTag

--- react/components/FilterNavigatorTitleTag.js
+++ react/components/FilterNavigatorTitleTag.js
@@ -6,36 +6,13 @@ const CSS_HANDLES = ['filterMessage']
 
 const FilterNavigatorTitleTags = ({ filtersTitleTag }) => {
   const handles = useCssHandles(CSS_HANDLES)
+  const CustomTag = filtersTitleTag
 
-  if (filtersTitleTag === 'h1') {
+  if (CustomTag && filtersTitleTag !== 'h5') {
     return (
-      <h1 className={`${handles.filterMessage} t-heading-1 mv5`}>
+      <CustomTag className={`${handles.filterMessage} mv5`}>
         <FormattedMessage id="store/search-result.filter-button.title" />
-      </h1>
-    )
-  }
-
-  if (filtersTitleTag === 'h2') {
-    return (
-      <h2 className={`${handles.filterMessage} t-heading-2 mv5`}>
-        <FormattedMessage id="store/search-result.filter-button.title" />
-      </h2>
-    )
-  }
-
-  if (filtersTitleTag === 'h3') {
-    return (
-      <h3 className={`${handles.filterMessage} t-heading-3 mv5`}>
-        <FormattedMessage id="store/search-result.filter-button.title" />
-      </h3>
-    )
-  }
-
-  if (filtersTitleTag === 'h4') {
-    return (
-      <h4 className={`${handles.filterMessage} t-heading-4 mv5`}>
-        <FormattedMessage id="store/search-result.filter-button.title" />
-      </h4>
+      </CustomTag>
     )
   }
 
