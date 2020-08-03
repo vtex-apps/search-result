0a1e7176d81ade7ad59c712a745fdfb9f5979671
Ronaldo Caetano <ronaldogodoi2@gmail.com>
2020-08-03 19:50:47 -0300

    fixing code structure and readme

--- react/components/FilterNavigatorTitleTag.js
+++ react/components/FilterNavigatorTitleTag.js
@@ -4,22 +4,18 @@ import { FormattedMessage } from 'react-intl'
 
 const CSS_HANDLES = ['filterMessage']
 
-const FilterNavigatorTitleTags = ({ filtersTitleHtmlTag }) => {
+const FilterNavigatorTitleTags = ({ filtersTitleHtmlTag = 'h5' }) => {
   const handles = useCssHandles(CSS_HANDLES)
   const CustomTag = filtersTitleHtmlTag
 
-  if (CustomTag && filtersTitleHtmlTag !== 'h5') {
-    return (
-      <CustomTag className={`${handles.filterMessage} mv5`}>
-        <FormattedMessage id="store/search-result.filter-button.title" />
-      </CustomTag>
-    )
-  }
-
   return (
-    <h5 className={`${handles.filterMessage} t-heading-5 mv5`}>
+    <CustomTag
+      className={`${handles.filterMessage} ${
+        filtersTitleHtmlTag === 'h5' ? 't-heading-5' : ''
+      } mv5`}
+    >
       <FormattedMessage id="store/search-result.filter-button.title" />
-    </h5>
+    </CustomTag>
   )
 }
 
