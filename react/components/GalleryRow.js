1c38c5aa3f2e0760e73d52f54fed401912c14b05
Lucas Bebber <lucasbbebber@gmail.com>
2020-09-07 16:32:18 -0300

    Use useRenderOnView hook for lazy rendering GalleryRows

--- react/components/GalleryRow.js
+++ react/components/GalleryRow.js
@@ -1,5 +1,5 @@
-import React, { useRef, memo, useState } from 'react'
-import { useOnView } from '../hooks/useOnView'
+import React, { memo } from 'react'
+import { useRenderOnView } from '../hooks/useRenderOnView'
 import { useCssHandles, applyModifiers } from 'vtex.css-handles'
 import classNames from 'classnames'
 
@@ -14,57 +14,37 @@ const GalleryRow = ({
   itemsPerRow,
   lazyRender,
 }) => {
-  const dummy = useRef()
   const handles = useCssHandles(CSS_HANDLES)
-  const [hasBeenViewed, setHasBeenViewed] = useState(!lazyRender)
 
   const style = {
     flexBasis: `${100 / itemsPerRow}%`,
     maxWidth: `${100 / itemsPerRow}%`,
   }
 
-  /** if lazyRender is true, only renders the actual content once "dummy"
-   * is inside the viewport
-   */
-  useOnView({
-    ref: dummy,
-    onView: () => setHasBeenViewed(true),
-    once: true,
-    initializeOnInteraction: true,
-  })
+  const { hasBeenViewed, dummyElement } = useRenderOnView({ lazyRender })
 
-  return hasBeenViewed ? (
-    products.map(product => {
-      return (
-        <div
-          key={product.productId}
-          style={style}
-          className={classNames(
-            applyModifiers(handles.galleryItem, displayMode),
-            'pa4'
-          )}
-        >
-          <GalleryItem
-            item={product}
-            summary={summary}
-            displayMode={displayMode}
-          />
-        </div>
-      )
-    })
-  ) : (
-    <div
-      ref={dummy}
-      style={{
-        width: '100%',
-        height: 400,
-        position: 'relative',
-        /** Pulls the object 200px up so it renders a bit earlier,
-         * before the user would actually see the content */
-        top: -200,
-      }}
-    />
-  )
+  if (!hasBeenViewed) {
+    return dummyElement
+  }
+
+  return products.map(product => {
+    return (
+      <div
+        key={product.productId}
+        style={style}
+        className={classNames(
+          applyModifiers(handles.galleryItem, displayMode),
+          'pa4'
+        )}
+      >
+        <GalleryItem
+          item={product}
+          summary={summary}
+          displayMode={displayMode}
+        />
+      </div>
+    )
+  })
 }
 
 export default memo(GalleryRow)
