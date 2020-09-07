7639b326d258bf50a3a7f491e10610b24f09c272
Lucas Bebber <lucasbbebber@gmail.com>
2020-09-07 18:20:51 -0300

    Add getSettings mock

--- react/__tests__/Gallery.test.js
+++ react/__tests__/Gallery.test.js
@@ -6,6 +6,17 @@ import { products, summary } from 'GalleryMocks'
 
 import Gallery from '../Gallery'
 
+import { useRuntime } from '../__mocks__/vtex.render-runtime'
+const mockUseRuntime = useRuntime
+
+beforeEach(() => {
+  jest.clearAllMocks()
+
+  mockUseRuntime.mockImplementation(() => ({
+    getSettings: () => ({}),
+  }))
+})
+
 describe('<Gallery />', () => {
   const renderComponent = customProps => {
     const props = {
