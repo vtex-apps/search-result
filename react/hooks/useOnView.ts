b3b2dba058ee89323d2066ff48387f76bdfdc078
Lucas Bebber <lucasbbebber@gmail.com>
2020-09-07 16:30:46 -0300

    Add comment re origin of useOnView

--- react/hooks/useOnView.ts
+++ react/hooks/useOnView.ts
@@ -1,3 +1,4 @@
+/** Code borrowed from vtex.render-runtime. Maybe it should export this in the future */
 import { useEffect, useRef, MutableRefObject } from 'react'
 
 interface IntersectionEvent {
