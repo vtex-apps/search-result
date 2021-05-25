391ccc26be2e6c42867d57c8a6092bb80adc5738
Gabi Danciu <gabi.danciu@webtoclick.com>
2021-05-25 13:14:08 +0300

    adding prop to disply filter like on mobile even if you are on desktop

--- react/FilterNavigator.js
+++ react/FilterNavigator.js
@@ -33,6 +33,7 @@ const CSS_HANDLES = [
 const LAYOUT_TYPES = {
   responsive: 'responsive',
   desktop: 'desktop',
+  phone: 'phone'
 }
 
 const getSelectedCategories = (tree) => {
@@ -94,8 +95,9 @@ const FilterNavigator = ({
 
   const mobileLayout =
     (isMobile && layout === LAYOUT_TYPES.responsive) ||
-    layout === LAYOUT_TYPES.mobile
-
+    layout === LAYOUT_TYPES.mobile ||
+    !isMobile && layout === LAYOUT_TYPES.phone || isMobile && layout === LAYOUT_TYPES.phone
+ 
   useEffect(() => {
     // This condition confirms if there are facets that still need fetching
     const needsFetching = !!filters.find(
