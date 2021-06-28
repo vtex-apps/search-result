3c97ca8d50c9f12debdd4722562f4d94b624b669
Cristian Longo <cristian.longo@customsoft.ro>
2021-06-28 13:29:22 +0300

    add navigateToFacet

--- react/components/AvailableFilters.js
+++ react/components/AvailableFilters.js
@@ -38,6 +38,7 @@ const Filter = ({
   appliedFiltersOverview,
   showClearByFilter,
   priceRangeLayout,
+  scrollToTop,
 }) => {
   const { type, title, facets, quantity, oneSelectedCollapse = false } = filter
 
