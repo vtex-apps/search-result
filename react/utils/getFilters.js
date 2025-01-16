5af0500109543e3b052b4cde690e55b14ff22345
Arthur Ferrão <arthur.ferrao@vtex.com>
2025-01-16 16:28:23 -0300

    feat: update condition to enable delivery promises

--- react/utils/getFilters.js
+++ react/utils/getFilters.js
@@ -67,7 +67,7 @@ const getFilters = ({
 
   if (
     !showShippingFacet ||
-    !(production && variant && variant.indexOf('delivery_promises') !== -1)
+    (production && variant && variant.indexOf('delivery_promises') === -1)
   ) {
     deliveriesFormatted = deliveriesFormatted.filter(
       d => d.name !== SHIPPING_KEY
