2a5aad2b205ccc27c4375e0d1a1210b5cad7dc35
Arthur Ferrão <arthur.ferrao@vtex.com>
2025-03-25 15:54:18 -0300

    feat: add pixel event to radio button click

--- react/components/AccordionFilterGroup.js
+++ react/components/AccordionFilterGroup.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useIntl } from 'react-intl'
 import classNames from 'classnames'
 import { useCssHandles, applyModifiers } from 'vtex.css-handles'
+import { useSearchPage } from 'vtex.search-page-context/SearchPageContext'
+import { usePixel } from 'vtex.pixel-manager'
 
 import AccordionFilterItem from './AccordionFilterItem'
 import FacetCheckboxList from './FacetCheckboxList'
@@ -9,6 +11,7 @@ import useSelectedFilters from '../hooks/useSelectedFilters'
 import { getFilterTitle } from '../constants/SearchHelpers'
 import { searchSlugify } from '../utils/slug'
 import RadioFilters from './RadioFilters'
+import { pushFilterManipulationPixelEvent } from '../utils/filterManipulationPixelEvents'
 
 const CSS_HANDLES = ['accordionFilterOpen']
 
@@ -30,6 +33,7 @@ const AccordionFilterGroup = ({
   onClearFilter,
   showClearByFilter,
 }) => {
+  const { searchQuery } = useSearchPage()
   const handles = useCssHandles(CSS_HANDLES)
   const filters = useSelectedFilters(facets)
   const selectedFilters = filters.filter(facet => facet.selected)
@@ -37,6 +41,7 @@ const AccordionFilterGroup = ({
   const facetTitle = getFilterTitle(title, intl)
   const slugifiedFacetTitle = searchSlugify(facetTitle)
   const facetKey = filters.length > 0 ? filters[0].key : null
+  const { push } = usePixel()
 
   const isRadio = filters.some(filter => filter.key === 'shipping')
 
@@ -65,6 +70,12 @@ const AccordionFilterGroup = ({
           <RadioFilters
             facets={filters}
             onChange={facet => {
+              pushFilterManipulationPixelEvent({
+                name: facetTitle,
+                value: facet.name,
+                products: searchQuery?.products ?? [],
+                push,
+              })
               onFilterCheck({ ...facet, title: facetTitle }, true)
             }}
           />
