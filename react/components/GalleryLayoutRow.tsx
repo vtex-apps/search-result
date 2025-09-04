95cdde7b87452ff16dcefe8bf21d2a7d0e8a0d7a
Thiago Pereira <thiago.pereira@vtex.com>
2025-09-04 10:34:33 -0300

    refactor: update data attributes in GalleryLayoutRow to use product position

--- react/components/GalleryLayoutRow.tsx
+++ react/components/GalleryLayoutRow.tsx
@@ -65,8 +65,7 @@ const GalleryLayoutRow: React.FC<GalleryLayoutRowProps> = ({
           <div
             data-af-onclick={searchId ? true : undefined}
             data-af-search-id={searchId}
-            data-af-product-link={product?.link}
-            data-af-product-id={product?.productId}
+            data-af-product-position={index + 1}
             key={product.cacheId}
             style={style}
             className={classNames(
